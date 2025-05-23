f4bf08b0f9dd03743e740cab087b66503a4960cc
BegencFrontDev <begench.d.2004@gmail.com>
2025-05-23 07:28:26 +0500

    edit user complated

--- js/formRules.js
+++ js/formRules.js
@@ -34,3 +34,20 @@ const recoveryPasswordFormValidationRules = {
     errorMessage: "Неверная почта:",
   },
 };
+
+const editUserFormValidationRules = {
+  email: {
+    validate: (value) => {
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      return emailRegex.test(value);
+    },
+    errorMessage: "Неверная почта:",
+  },
+  user: {
+    validate: (value) => {
+      if (value.length < 4) return false;
+      return true;
+    },
+    errorMessage: "Неверное имя пользователя:",
+  },
+};
